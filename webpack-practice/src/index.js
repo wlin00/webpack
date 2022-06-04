d97d1cc8a4cc08c467e7b1a2d684e509c98f6ae6
zhongjunji <zhongjunji@jd.com>
2022-06-05 03:03:50 +0800

    feat:webpack5支持ie

--- webpack-practice/src/index.js
+++ webpack-practice/src/index.js
@@ -1,2 +1,11 @@
-const str = 'hi'
-console.log(str)
+import { a } from './a' // 直接引入a
+const b = import('./b') // 动态引入b
+
+const fn = () => {
+  console.log('test')
+  console.log('a', a)
+  console.log('b', b)
+  console.log(Promise.resolve('test promise'))
+}
+
+fn()
