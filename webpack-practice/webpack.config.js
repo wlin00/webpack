6951950b267a1a6c051430aa13de47ad1a2c18bb
zhongjunji <zhongjunji@jd.com>
2022-06-05 16:53:13 +0800

    feat:使用babel-loader处理js/jsx

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  mode: 'production'
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        // 用babel-loader来处理js/jsx文件，而非用webpack默认能力打包
+        // 这样方便拓展更多能力
+        test: /\.jsx?$/,
+        exclude: /node_modules/, // 遇到node_modules文件不处理，因为这些文件都默认打包过
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'] // 使用babel-loader预设能力处理js/jsx文件，env是根据环境自动变化的一个推荐包
+            ]
+          }
+        }
+      }
+    ]
+  }
 }
