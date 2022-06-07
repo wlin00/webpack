d64f4aa96d14c57b08cebfe673249e562c5fafa9
zhongjunji <zhongjunji@jd.com>
2022-06-07 21:18:15 +0800

    add:配置less-loader，让webpack支持打包less文件 & 导出less变量 & 全局less文件引入指定less

--- webpack-practice/src/index.js
+++ webpack-practice/src/index.js
@@ -2,19 +2,19 @@ import { a } from '@/a' // 直接引入a
 import { jsxDemo } from '@/jsx-demo.jsx';
 import { tsDemo } from '@/ts-demo.ts'
 import { tsxDemo } from '@/tsx-demo.tsx';
+import varsCss from '@/css-demo.css'
+// test - scss
 import '@/scssDemo.scss'
 import vars from '@/scss-export.scss'
-import varsCss from '@/css-demo.css'
+// test - less
+import '@/lessDemo.less'
+import varsLess from '@/less-export.less'
 
 
 const b = import('@/b') // 动态引入b
 console.log('vars', vars)
 console.log('vars-css', varsCss)
-
-console.log('get - jsx', jsxDemo)
-console.log('get - ts', tsDemo)
-console.log('get - tsx', tsxDemo)
-
+console.log('vars-Less', varsLess)
 
 const fn = () => {
   console.log('test')
--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -52,6 +52,25 @@ module.exports = {
           }
         }]
       },
+      { // 处理less文件，loader处理顺序, less-loader -> css-loader -> style-loader
+        test: /\.less$/i, // 不区分大小写的匹配less
+        use: ['style-loader', {
+          loader: 'css-loader', // 额外配置css-loader，将解析类型改为icss，从而支持js读取css导出变量
+          options: {
+            modules: {
+              compileType: 'icss'
+            }
+          }
+        }, {
+          loader: 'less-loader',
+          options: {
+            additionalData: `@import '@/less-vars.less';`, // 全局注入scss-vars.scss
+            lessOptions: { // 若不配置alias，引入文件基于当前目录__dirname
+              includePaths: [__dirname]
+            }
+          }
+        }]
+      }, 
       { // 处理css文件，loader处理顺序, css-loader -> style-loader
         test: /\.css$/i, // 不区分大小写的匹配scss / sass
         use: ['style-loader', {
