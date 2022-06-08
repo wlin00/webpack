adebb4ae338d19da34c3378f4c1d7bb2c26cbf38
zhongjunji <zhongjunji@jd.com>
2022-06-08 21:08:59 +0800

    feat:HtmlWebpackPlugin、MiniCssExtractPlugin来自动生成HTML页面 + 生产环节单独提取css文件；css和js文件添加hash；代码优化

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -1,15 +1,45 @@
 const EsLintPlugin = require('eslint-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const mode = 'production'
+
+// 简化代码 - 使用抽离函数统一处理css/scss/less
+const useStyleLoader = (...loader) => [
+  // 参数1根据当前环境判断，若是生产环境，则单独提取css文件为额外bundle
+  mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+  { //提取css文件为单独bundle，在emitAssets阶段多导出一个css
+    loader: 'css-loader', // 额外配置css-loader，将解析类型改为icss，从而支持js读取css导出变量
+    options: {
+      modules: {
+        compileType: 'icss'
+      }
+    }
+  }, 
+  ...loader // 根据入参loader处理
+]
 
 module.exports = {
-  mode: 'production',
+  mode,
+  output: {
+    filename: '[name].[contenthash].js', // 为输出的js添加hash，记得使用cleanWebpackPlugin 每次emit阶段清除dist
+    path: path.resolve(__dirname, './dist')
+  },
   plugins: [
-    new CleanWebpackPlugin(), // emit阶段写文件前清空output目录，done阶段清空assets目录无用、过期的文件；
-    new EsLintPlugin( // 让webpack打包时，能使用Eslint去找到代码中的错误
+    // emit阶段写文件前清空output目录，done阶段清空assets目录无用、过期的文件；
+    new CleanWebpackPlugin(), 
+    // 让webpack打包时，能使用Eslint去找到代码中的错误
+    new EsLintPlugin( 
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] }
     ), 
-  ],
+    // 自动生成html页面
+    new HtmlWebpackPlugin(),
+    // 单独提取css文件，需要在对应css-loader前将MiniCssExtractPlugin.loader替换style-loader
+    mode === 'production' && new MiniCssExtractPlugin({ 
+      filename: '[name].[contenthash].css' // 为css添加hash
+    }), 
+  ].filter(Boolean), // 过滤plugins中为false（即未开启）的选项，这样的写法给各个插件提供了开关的功能
   resolve: {
     alias: { // 若webpack发现@符号，则默认找到当前目录下src
       '@': path.resolve(__dirname, './src/')
@@ -35,14 +65,7 @@ module.exports = {
       },
       { // 处理scss文件，loader处理顺序, sass-loader -> css-loader -> style-loader
         test: /\.s[ac]ss$/i, // 不区分大小写的匹配scss / sass
-        use: ['style-loader', {
-          loader: 'css-loader', // 额外配置css-loader，将解析类型改为icss，从而支持js读取css导出变量
-          options: {
-            modules: {
-              compileType: 'icss'
-            }
-          }
-        }, {
+        use: useStyleLoader({
           loader: 'sass-loader',
           options: {
             additionalData: `@import '~@/scss-vars.scss';`, // 全局注入scss-vars.scss
@@ -50,18 +73,11 @@ module.exports = {
               includePaths: [__dirname]
             }
           }
-        }]
+        })
       },
       { // 处理less文件，loader处理顺序, less-loader -> css-loader -> style-loader
         test: /\.less$/i, // 不区分大小写的匹配less
-        use: ['style-loader', {
-          loader: 'css-loader', // 额外配置css-loader，将解析类型改为icss，从而支持js读取css导出变量
-          options: {
-            modules: {
-              compileType: 'icss'
-            }
-          }
-        }, {
+        use: useStyleLoader({
           loader: 'less-loader',
           options: {
             additionalData: `@import '~@/less-vars.less';`, // 全局注入less-vars.less
@@ -69,18 +85,11 @@ module.exports = {
               includePaths: [__dirname]
             }
           }
-        }]
+        })
       }, 
       { // 处理css文件，loader处理顺序, css-loader -> style-loader
         test: /\.css$/i, // 不区分大小写的匹配scss / sass
-        use: ['style-loader', {
-          loader: 'css-loader', // 额外配置css-loader，将解析类型改为icss，从而支持js读取css导出变量
-          options: {
-            modules: {
-              compileType: 'icss'
-            }
-          }
-        }]
+        use: useStyleLoader()
       }
     ]
   }
